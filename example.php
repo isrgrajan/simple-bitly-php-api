61680341ea7d1a5325e7af84c17e26efc7df3425
Isrg <isrgrajan@outlook.com>
2020-04-08 20:53:53 +0530

    Update example.php
    syntax fix

--- example.php
+++ example.php
@@ -3,6 +3,6 @@ include("./isrg-bitly/api.php");
 
 $genericAccessToken="your-bitly-generic-AccessToken-"; // See: https://bitly.is/2UQ3XLo
 $full_url="https://www.google.com"; 
-$short_url=get_short_url($full_url, $genericAccessToken)
+$short_url=get_short_url($full_url, $genericAccessToken);
 
 echo $short_url;
